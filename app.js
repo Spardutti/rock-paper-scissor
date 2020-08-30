49d1789c9d56cfeca1ad111ce82b25a1499932d9
spardutti <luisdamian.sp@gmail.com>
2020-08-30 16:07:10 -0300

    functional game with buttons and DOM elements to a score of 5, no reset button

--- app.js
+++ app.js
@@ -14,58 +14,95 @@ function computerPlay() {
 //set both scores to 0
 let playerScr = 0;
 let ComputerScr = 0;
-//create a func that will play each round and add 1 to the winner
-function roundPlay(playerSelection, computerSelection) {
-  if (playerSelection == "rock" && computerSelection == "scissor") {
-    playerScr++;
-    return "Rock beat scissors! Player Wins this round";
-  }
-  if (playerSelection == "paper" && computerSelection == "rock") {
-    playerScr++;
-    return "Paper beat rock! Player Wins this round";
-  }
-  if (playerSelection == "scissor" && computerSelection == "paper") {
-    playerScr++;
-    return "Scissor beat paper! Player Wins this round!";
-  }
-  if (computerSelection == "rock" && playerSelection == "scissor") {
-    ComputerScr++;
-    return "Rock beat scissor! Computer Wins this round";
-  }
-  if (computerSelection == "paper" && playerSelection == "rock") {
-    ComputerScr++;
-    return "Paper beat rock! Computer Wins this round";
-  }
-  if (computerSelection == "scissor" && playerSelection == "paper") {
-    ComputerScr++;
-    return "Scissor beat paper! Computer Wins this round";
-  } else return "Draw";
-}
-// game will repeat 5 times and check who have won more rounds to pick a winner
-function game() {
-  for (let round = 1; round <= 5; round++) {
-    playerSelection = prompt("Choose rock paper or scissor").toLowerCase();
-    //check if user used the correct words
-    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissor") {
-      computerSelection = computerPlay();
-      console.log(roundPlay(playerSelection, computerSelection));
-      console.log(playerScr, ComputerScr);
+let pSelec = document.querySelector(".pSelec");
+let cSelec = document.querySelector(".cSelec");
+let pScore = document.querySelector(".pScore");
+let cScore = document.querySelector(".cScore");
+
+
+function rockBtn() {
+  if (playerScr < 5 && ComputerScr < 5) {
+      pSelec.textContent = "Rock";
+
+    let computerChoice = computerPlay();
+    if (computerChoice == "rock") {
+      cSelec.textContent = "Rock";
+    } else if (computerChoice == "paper") {
+      cSelec.textContent = "Paper";
+      ComputerScr++;
+      cScore.textContent = ComputerScr;
     } else {
-      //if the input is wrong print an alert and dont advance the round count
-      playerSelection = alert("Please enter ROCK PAPER or SCISSOR only");
-      round--;
+      cSelec.textContent = "Scissor";
+      playerScr++;
+      pScore.textContent = playerScr;
+    }
+  } if (playerScr == 5) {
+    pSelec.textContent = "Player Win";
+  }
+  if (ComputerScr == 5) {
+    cSelec.textContent = "Computer Wins";
+  }
+  
+  
+}
+function paperBtn() {
+  if (playerScr < 5 && ComputerScr < 5) {
+    pSelec.textContent = "Paper";
+
+    let computerChoice = computerPlay();
+    if (computerChoice == "rock") {
+      cSelec.textContent = "Rock";
+      playerScr++;
+      pScore.textContent = playerScr;
+    } else if (computerChoice == "paper") {
+      cSelec.textContent = "Paper";
+    } else {
+      cSelec.textContent = "Scissor";
+      ComputerScr++;
+      cScore.textContent = ComputerScr;
     }
   }
-  //if there is a  draw on scores, play one more round
-  if (playerScr == ComputerScr) {
-    console.log("We have a tie: " + playerScr + " to " + ComputerScr);
+  if (playerScr == 5) {
+    pSelec.textContent = "Player Win";
   }
-  //check for winner
-  else if (playerScr > ComputerScr) {
-    console.log("Player wins " + playerScr + " to " + ComputerScr);
-  } else {
-    console.log("Computer wins: " + ComputerScr + " to " + playerScr);
+  if (ComputerScr == 5) {
+    cSelec.textContent = "Computer Wins";
   }
+  
+ 
 }
-//log the game to the console
-console.log(game());
+function scissorBtn(){
+  if (playerScr < 5 && ComputerScr < 5) {
+    pSelec.textContent = "Scissor";
+
+    let computerChoice = computerPlay();
+    if (computerChoice == "rock") {
+      cSelec.textContent = "Rock";
+      ComputerScr++;
+      cScore.textContent = ComputerScr;
+    } else if (computerChoice == "paper") {
+      cSelec.textContent = "Paper";
+      playerScr++;
+      pScore.textContent = playerScr;
+    } else {
+      cSelec.textContent = "Scissor";
+    }
+  }
+  if (playerScr == 5) {
+    pSelec.textContent = "Player Win";
+  } if (ComputerScr == 5) {
+    cSelec.textContent = "Computer Wins"
+  }
+  
+}
+
+  const rock = document.querySelector(".btn-rock");
+  rock.addEventListener("click", rockBtn, 
+  );
+  const paper = document.querySelector(".btn-paper");
+  paper.addEventListener("click", paperBtn
+  );
+  const scissor = document.querySelector(".btn-scissor");
+  scissor.addEventListener("click", scissorBtn);
+
+
